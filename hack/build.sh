bd796c802789c36386600c8e050f159142a4aa4c
TylrA <tylerhaddams@gmail.com>
2023-06-29 11:22:29 -0600

    create build.sh

---
+++ hack/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+[[ -z "$DOCKER_BIN" ]] && DOCKER_BIN=docker
+[[ -z "$VERSION" ]] && VERSION=$(printf "%s%s" "$(date '+%Y-%m-%d-')" "$(git rev-parse --short HEAD)")
+if [[ -z "$1" && -z "$REPOSITORY" ]]; then
+    echo "provide a repository name (as <registry>/<repository>) or define REPOSITORY"
+    exit 1
+fi
+
+PROJ_DIR="$(git rev-parse --show-toplevel)"
+
+TAG="${REPOSITORY}:${VERSION}"
+
+if [[ ! $DOCKER_BIN build --platform linux/amd64 -t "$TAG" $PROJ_DIR ]]; then
+    exit 1
+fi
+
+IMAGE_ID=$($DOCKER_BIN images --no-trunc --quiet $TAG)
+$DOCKER_BIN push $IMAGE_ID $TAG
